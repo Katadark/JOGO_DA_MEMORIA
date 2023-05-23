416c933428f94211469320768ae18f2776d96ada
Jefferson Alves <jeeffsantoos@outlook.com>
2023-05-22 22:38:00 -0300

    Estrutura base

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame, cv2, random, os
+
+pygame.init()
+
+window_width = 1280
+window_height = 860
+screen = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption('Memory Game')
+
+pygame.quit()
