5671abce011492c87a8368342ae13f1ce2787f85
Katadark <mimiscel@gmail.com>
2023-05-30 20:31:59 -0300

    fazendo a janelinha

--- main.py
+++ main.py
@@ -7,4 +7,17 @@ window_height = 860
 screen = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Memory Game')
 
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+
+FPS = 60
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+         running = False
+
 pygame.quit()
