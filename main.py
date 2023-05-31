9e7de583c374a4af6bbe3befff0ed96cb4b308e3
Katadark <mimiscel@gmail.com>
2023-05-31 16:05:28 -0300

    adicionando video de fundo, operação de play e stop na musica e video.

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ class Game():
         self.level_complete = False  
         
         
-        #inciciando o video 
+        #adicionando icones e inicializando video
         self.is_video_playing = True
         self.play = pygame.image.load('images/play.png').convert_alpha()
         self.stop = pygame.image.load('images/stop.png').convert_alpha()
         self.video_toggle = self.play
         self.video_toggle_rect = self.video_toggle.get_rect(topright = (WINDOW_WIDTH - 50, 10))
+        self.get_video()
     
         
         #iniciando a musica:
@@ -30,7 +31,35 @@ class Game():
         
     
     def update(self,event_list): 
+        if self.is_video_playing:
+           self.sucess, self.img = self.cap.read()
+        self.user_imput(event_list)
         self.draw()
+        
+       #eventos do mouse com o video
+    def user_imput(self, event_list):
+       for event in event_list:
+         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: 
+            if self.music_toggle_rect.collidepoint(pygame.mouse.get_pos()):
+              if self.is_music_playing:
+                 self.is_music_playing = False
+                 self.music_toggle = self.sound_off
+                 pygame.mixer.music.pause()
+              else:
+                self.is_music_playing = True
+                self.music_toggle = self.sound_on
+                pygame.mixer.music.unpause()
+                
+            if self.video_toggle_rect.collidepoint(pygame.mouse.get_pos()):
+              if self.is_video_playing:
+                  self.is_video_playing = False
+                  self.video_toggle = self.stop
+              else:
+                self.is_video_playing = True
+                self.video_toggle = self.play
+                
+                   
+      
       
     def draw(self):
         screen.fill(BLACK)
@@ -50,6 +79,13 @@ class Game():
         info_text = content_font.render('Selecione 2 de cada: ', True, WHITE)
         info_rect = info_text.get_rect(midtop = (WINDOW_WIDTH //2, 120))
         
+        
+        if self.is_video_playing:
+          if  self.sucess:
+             screen.blit(pygame.image.frombuffer(self.img.tobytes(), self.shape, 'BGR'), (0, 120))
+          else:
+             self.get_video()
+        
         if not self.level == 5:
           next_text = content_font.render('Nível completo. Pressione espaço para o próximo nível', True, WHITE)
         else: 
@@ -65,7 +101,13 @@ class Game():
         
         if self.level_complete:
            screen.blit(next_text, next_rect)
-    
+   
+      #inserindo video de fundo
+    def get_video(self):
+      self.cap = cv2.VideoCapture('videos/bgvideo.mp4')
+      self.sucess, self.img = self.cap.read()
+      self.shape = self.img.shape[1::-1]
+  
 pygame.init()
 
 WINDOW_WIDTH = 1280
@@ -85,12 +127,12 @@ game =Game()
 
 running = True
 while running:
-    game_list = pygame.event.get()
-    for event in game_list:
+    event_list = pygame.event.get()
+    for event in event_list:
         if event.type == pygame.QUIT:
             running = False
          
-    game.update(game_list)
+    game.update(event_list)
     
     
     pygame.display.update()
